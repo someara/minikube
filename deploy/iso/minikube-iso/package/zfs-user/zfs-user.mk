0eb232ab6eb6e1b372b0f6aed364c43511e6275b
Sean OMeara <sean@sean.io>
2026-03-06 20:52:54 +0100

    Rename ZFS package to zfs-user to avoid collision with upstream Buildroot
    Buildroot master now ships its own package/zfs/ which requires
    BR2_LINUX_KERNEL=y and builds kernel modules. Our package only builds
    userspace tools (--with-config=user) since ZFS is compiled statically
    into the kernel. Rename to zfs-user to avoid the 'Package defined a
    second time' error.

--- deploy/iso/minikube-iso/package/zfs-user/zfs-user.mk
+++ deploy/iso/minikube-iso/package/zfs-user/zfs-user.mk
@@ -1,23 +1,26 @@
 ################################################################################
 #
-# zfs (userspace only)
+# zfs-user (userspace only)
 #
 # OpenZFS userspace utilities: zpool, zfs, zed, zdb, etc.
 # The kernel module is compiled statically into the kernel (CONFIG_ZFS=y),
 # so this package only builds the userspace tools and libraries.
 #
+# Named zfs-user to avoid collision with Buildroot's upstream zfs package
+# (which requires BR2_LINUX_KERNEL=y and builds kernel modules).
+#
 ################################################################################
 
-ZFS_VERSION = 2.4.1
-ZFS_SOURCE = zfs-$(ZFS_VERSION).tar.gz
-ZFS_SITE = https://github.com/openzfs/zfs/releases/download/zfs-$(ZFS_VERSION)
-ZFS_LICENSE = CDDL
-ZFS_LICENSE_FILES = LICENSE
-ZFS_CPE_ID_VENDOR = openzfs
-ZFS_CPE_ID_PRODUCT = openzfs
-ZFS_INSTALL_STAGING = YES
+ZFS_USER_VERSION = 2.4.1
+ZFS_USER_SOURCE = zfs-$(ZFS_USER_VERSION).tar.gz
+ZFS_USER_SITE = https://github.com/openzfs/zfs/releases/download/zfs-$(ZFS_USER_VERSION)
+ZFS_USER_LICENSE = CDDL
+ZFS_USER_LICENSE_FILES = LICENSE
+ZFS_USER_CPE_ID_VENDOR = openzfs
+ZFS_USER_CPE_ID_PRODUCT = openzfs
+ZFS_USER_INSTALL_STAGING = YES
 
-ZFS_DEPENDENCIES = \
+ZFS_USER_DEPENDENCIES = \
 	host-pkgconf \
 	util-linux \
 	zlib \
@@ -30,7 +33,7 @@ ZFS_DEPENDENCIES = \
 # --disable-sysvinit: systemd only, no sysvinit scripts
 # --with-udevdir: udev rules location
 # --with-systemdunitdir / --with-systemdpresetdir: systemd unit locations
-ZFS_CONF_OPTS = \
+ZFS_USER_CONF_OPTS = \
 	--with-config=user \
 	--disable-static \
 	--disable-pyzfs \
@@ -44,7 +47,7 @@ ZFS_CONF_OPTS = \
 	--with-mounthelperdir=/usr/sbin
 
 # ZFS configure needs to find libuuid and libblkid via pkg-config
-ZFS_CONF_ENV = \
+ZFS_USER_CONF_ENV = \
 	PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig:$(STAGING_DIR)/usr/share/pkgconfig"
 
 $(eval $(autotools-package))
